3b72413f1d75963aef49192f002c493c2ff9b250
Keshav <kkarki1998@gmail.com>
2023-03-14 16:29:28 +0530

    first commit

---
+++ pads.sql
@@ -0,0 +1,7 @@
+SELECT
+    concat(Name, concat('(',substring(Occupation, 1,1) ,')'))
+    as c FROM OCCUPATIONS
+UNION
+    (select concat('There are a total of ', count(Occupation),' ', lower(Occupation),'s.') as c
+FROM OCCUPATIONS group by Occupation) order by c;
+
